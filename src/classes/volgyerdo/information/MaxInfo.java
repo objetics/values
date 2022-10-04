0f2f20791b866d1dec7c0db89a40eb31c250cb39
Pőcze Zsolt <pocze.z@gmail.com>
2022-10-04 22:33:01 +0200

    Change for publication.

--- src/classes/volgyerdo/information/MaxInfo.java
+++ src/classes/volgyerdo/information/MaxInfo.java
@@ -5,7 +5,6 @@
  */
 package volgyerdo.information;
 
-import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
@@ -15,12 +14,11 @@ import volgyerdo.commons.collection.CollectionUtils;
 import volgyerdo.commons.primitive.ArrayUtils;
 
 /**
- * Shannon Spectrum Minimum
  *
  * @author Volgyerdo Nonprofit Kft.
  */
-public class SSMInfo {
-
+public class MaxInfo {
+    
     public static double information(Object object) {
         byte[] array = ArrayUtils.toByteArray(object);
         return information(array);
@@ -66,42 +64,16 @@ public class SSMInfo {
         if (values.size() <= 1) {
             return 0;
         }
-        double minimumInfo = 0;
-        double absoluteMax = 0;
         Set atomicSet = new HashSet<>(values);
         int K = atomicSet.size();
         if (K == 1) {
             return 0;
         }
         int N = values.size();
-        double atomicInfo = ShannonEntropy.entropy(values);
-        absoluteMax = maxInformation(N, K, 1);
-
-        for (int r = 1; r <= N / 2; r++) {
-            List<List> parts = CollectionUtils.breakApart(values, r, false);
-            double maxInfo = maxInformation(N, K, r);
-            double actualInfo = ShannonInfo.information(parts);
-            actualInfo = maxInfo == 0 ? 0 : actualInfo / maxInfo * absoluteMax;
-            if (actualInfo == 0) {
-                actualInfo = atomicInfo * r;
-            }
-            if (minimumInfo == 0 || actualInfo < minimumInfo) {
-                minimumInfo = actualInfo;
-            }
-        }
-        return minimumInfo;
+        return N * Math.log(K) / Math.log(2);
     }
 
-    private static double maxInformation(int N, int K, int r) {
-        int m = N / r;
-        if (Math.pow(K, r) <= m) {
-            return N * Math.log(K) / Math.log(2);
-        } else {
-            return m * Math.log(m) / Math.log(2);
-        }
+    private MaxInfo() {
     }
-
-    private SSMInfo() {
-    }
-
+    
 }
